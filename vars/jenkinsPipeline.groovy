b58d015c0aa8fd5b3d754cc5bc37277425629a82
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-08-26 18:37:46 +0530

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -11,50 +11,53 @@ def call(String repoUrl , String branchName) {
            }
            stage("Build") {
                      parallel {
-					               stage('New SmartCID') {
-						                agent {
-							                 docker {
-								                  image 'maven:3-alpine'
-								                  args '-v $HOME/.m2:/root/.m2'
-								                  reuseNode true
-							                 }
-						                }
-						                steps {
-							                 script{STAGE_NAME="Build Stage"}
-							                 dir ('smartcid') {
-								                    sh 'mvn clean install -DskipTests '
-							                 }
-						                }
-					           }
+					stage('New SmartCID') {
+						agent {
+							docker {
+								image 'maven:3-alpine'
+								args '-v $HOME/.m2:/root/.m2'
+								reuseNode true
+							}
+						}
+						steps {
+							script{STAGE_NAME="Build Stage"}
+							dir ('smartcid') {
+								sh 'mvn clean install -DskipTests '
+							}
+						}
+					}
+		     }   
            }
            stage("Unit Test") {
-              when {
-					       expression { params.INCLUDE_UNIT_TESTING == true }
-				      }
-				        parallel {
-					         stage('Test - smartcid') {
-						         agent {
-							           docker {
-								            image 'maven:3-alpine'
-								            args '-v $HOME/.m2:/root/.m2'
-								            reuseNode true
-							           }
-						          }
-						          steps {
-							           script{STAGE_NAME="Unit Test"}
-							           dir ('smartcid') {
-								                  sh 'mvn test'
-								                  sh 'mvn speedy-spotless:install-hooks'
-								                  sh 'mvn speedy-spotless:check'
-							           }
-						          }
-				 		          post {
-							          always {
-								           junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
-							          }
-						          }
-					        }
-              }
+		   when {
+					expression { params.INCLUDE_UNIT_TESTING == true }
+				}
+				parallel {
+					stage('Test - smartcid') {
+						agent {
+							docker {
+								image 'maven:3-alpine'
+								args '-v $HOME/.m2:/root/.m2'
+								reuseNode true
+							}
+						}
+						steps {
+							script{STAGE_NAME="Unit Test"}
+							dir ('smartcid') {
+								sh 'mvn test'
+								sh 'mvn speedy-spotless:install-hooks'
+								sh 'mvn speedy-spotless:check'
+							}
+						}
+				 		post {
+							always {
+								junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+							}
+						}
+					}
+		    }
+                   
+           }
            
        }
    }
