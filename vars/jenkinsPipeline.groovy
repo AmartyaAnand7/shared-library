a9455f2a2fcfa7b17eadc233559e603fc963b50b
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-08-26 18:21:47 +0530

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -4,21 +4,57 @@ def call(String repoUrl) {
        stages {
            stage("Checkout Code") {
                steps {
-                   git branch: 'master',
+                   git branch: "${branchname}",
                        url: "${repoUrl}"
-                   echo "checked out"
+                  
                }
            }
            stage("Build") {
-               steps {
-                   echo "built"
-               }
+                     parallel {
+					               stage('New SmartCID') {
+						                agent {
+							                 docker {
+								                  image 'maven:3-alpine'
+								                  args '-v $HOME/.m2:/root/.m2'
+								                  reuseNode true
+							                 }
+						                }
+						                steps {
+							                 script{STAGE_NAME="Build Stage"}
+							                 dir ('smartcid') {
+								                    sh 'mvn clean install -DskipTests '
+							                 }
+						                }
+					           }
            }
            stage("Unit Test") {
-              steps {
-                   echo "tested"
-               }
-           }
+              when {
+					       expression { params.INCLUDE_UNIT_TESTING == true }
+				      }
+				        parallel {
+					         stage('Test - smartcid') {
+						         agent {
+							           docker {
+								            image 'maven:3-alpine'
+								            args '-v $HOME/.m2:/root/.m2'
+								            reuseNode true
+							           }
+						          }
+						          steps {
+							           script{STAGE_NAME="Unit Test"}
+							           dir ('smartcid') {
+								                  sh 'mvn test'
+								                  sh 'mvn speedy-spotless:install-hooks'
+								                  sh 'mvn speedy-spotless:check'
+							           }
+						          }
+				 		          post {
+							          always {
+								           junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+							          }
+						          }
+					        }
+              }
            
        }
    }
