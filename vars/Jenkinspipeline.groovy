b6adb39768b51358eed5ea3cf346b61f8a15c76a
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-08-26 11:01:11 +0530

    Update Jenkinspipeline.groovy

--- vars/Jenkinspipeline.groovy
+++ vars/Jenkinspipeline.groovy
@@ -1,1 +1,33 @@
-
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       stages {
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Build") {
+               steps {
+                   sh "mvn build"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
+}
