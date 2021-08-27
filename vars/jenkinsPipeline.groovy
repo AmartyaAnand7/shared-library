4fc7bc47b7eb598b54b54343c7d48dd2c0bb75e2
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-08-27 12:23:43 +0530

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -4,81 +4,69 @@ def call(String branchName , String repoUrl) {
        stages {
            stage("Checkout Code") {
                environment {
-					GITHUB_CREDENTIAL_ID = 'jenkins-operator'
-				}
-				
-						steps {
-							sh ' if [ -d "smartcid" ]; then rm -Rf "smartcid"; fi; mkdir smartcid'
-							dir ('smartcid') {
-								script{STAGE_NAME="Checkout Code"}
-								git branch: "${branchName.split("/")[2]}",
-								credentialsId: "${env.GITHUB_CREDENTIAL_ID}",
-								url: "${repoUrl}"
-							}
-						}
-					
-				
-           }
-           stage("Build") {
-                     
-						agent {
-							docker {
-								image 'maven:3-alpine'
-								args '-v $HOME/.m2:/root/.m2'
-								reuseNode true
-							}
-						}
-						steps {
-							script{STAGE_NAME="Build Stage"}
-							dir ('smartcid') {
-								sh 'mvn clean install -DskipTests '
-							}
-						}
-					
-           }
-           stage("Unit Test") {
-		   
-				
-						agent {
-							docker {
-								image 'maven:3-alpine'
-								args '-v $HOME/.m2:/root/.m2'
-								reuseNode true
-							}
-						}
-						steps {
-							script{STAGE_NAME="Unit Test"}
-							dir ('smartcid') {
-								sh 'mvn test'
-								sh 'mvn speedy-spotless:install-hooks'
-								sh 'mvn speedy-spotless:check'
-							}
-						}
-				 		post {
-							always {
-								junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
-							}
-						}
-					
-		                
-                   
-           }
-	   stage("Sonar Report") {
-		                                agent {
-							docker {
-								image 'maven:3-alpine'
-								args '-v $HOME/.m2:/root/.m2'
-								reuseNode true
-							}
-						}
-						steps {
-							dir ('smartcid') {
-								sh 'mvn -DskipTests sonar:sonar -Dsonar.host.url=http://16.107.50.87:8090 -Dsonar.exclusions=**/*.ts -Dsonar.analysis.mode=publish -Dsonar.projectName=SCID-NEW'
-							}
-						}
+			GITHUB_CREDENTIAL_ID = 'jenkins-operator'
+		}
+		steps {
+		    sh ' if [ -d "smartcid" ]; then rm -Rf "smartcid"; fi; mkdir smartcid'
+		    dir ('smartcid') {
+				script{STAGE_NAME="Checkout Code"}
+				git branch: "${branchName.split("/")[2]}",
+				credentialsId: "${env.GITHUB_CREDENTIAL_ID}",
+				url: "${repoUrl}"
+		    }
+		}
 	   }
-           
-       
+           stage("Build") {
+                     agent {
+			docker {
+			    image 'maven:3-alpine'
+			    args '-v $HOME/.m2:/root/.m2'
+			    reuseNode true
+			}
+		     }
+		     steps {
+		         script{STAGE_NAME="Build Stage"
+			 dir ('smartcid') {
+			     sh 'mvn clean install -DskipTests '
+			}
+		     }
+	   }
+           stage("Unit Test") {
+		  agent {
+		      docker {
+			image 'maven:3-alpine'
+			args '-v $HOME/.m2:/root/.m2'
+			reuseNode true
+		      }
+		  }
+		  steps {
+			script{STAGE_NAME="Unit Test"}
+			dir ('smartcid') {
+				sh 'mvn test'
+				sh 'mvn speedy-spotless:install-hooks'
+				sh 'mvn speedy-spotless:check'
+			}
+		  }
+		  post {
+		        always {
+			        junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+			}
+		  }
+	   }
+	   stage("Sonar Report") {
+		   agent {
+			docker {
+			        image 'maven:3-alpine'
+				args '-v $HOME/.m2:/root/.m2'
+				reuseNode true
+			}
+		   }
+		   steps {
+			dir ('smartcid') {
+				sh 'mvn -DskipTests sonar:sonar -Dsonar.host.url=http://16.107.50.87:8090 -Dsonar.exclusions=**/*.ts -Dsonar.analysis.mode=publish -Dsonar.projectName=SCID-NEW'
+			}
+		  }
+	   }
        }
    }
 }
