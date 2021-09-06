3ff1d0cdf1d852365278a174aed51ec784a15add
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-09-06 16:48:05 +0530

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -1,19 +1,17 @@
-def call(String repoUrl, String projectName) {
+def call(String repoUrl, String branchName, String projectName) {
     pipeline {
        agent any
        stages {
            stage("Checkout Code") {
                environment {
 			GITHUB_CREDENTIAL_ID = 'scid-jenkins-operator'
-		        BRANCH_NAME = 'develop'
-		        
 		}
 		steps {
 			sh " if [ -d ${projectName} ]; then rm -Rf ${projectName}; fi; mkdir ${projectName}"
 			dir ("${projectName}") {
 				script{STAGE_NAME="Checkout Code"}
 				git credentialsId: "${env.GITHUB_CREDENTIAL_ID}",
-				    branch: "${env.BRANCH_NAME}",
+				    branch: "${branchName}",
 				    url: "${repoUrl}"
 			}
 		}
