bca813d33cecaf9bd97e011f91bdbe7eb794d226
AmartyaAnand7 <80732246+AmartyaAnand7@users.noreply.github.com>
2021-09-08 14:42:49 +0530

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -1,7 +1,24 @@
 def call(String repoUrl, String branchName, String directoryName, String projectName) {
     pipeline {
        agent any
-       stages {
+       parameters {
+	    booleanParam(
+		name: 'INCLUDE_BUILD_STAGE',
+		defaultValue: true,
+		description: 'Tick if build has to be performed',
+	    )
+	    booleanParam(
+		name: 'INCLUDE_UNIT_TESTING',
+		defaultValue: true,
+		description: 'Tick if unit testing has to be performed',
+	    )
+	    booleanParam(
+		name: 'INCLUDE_SONAR_REPORT_STAGE',
+		defaultValue: true,
+		description: 'Tick if sonar stage has to be performed',
+	    )
+	}
+        stages {
            stage("Checkout Code") {
                environment {
 			GITHUB_CREDENTIAL_ID = 'scid-jenkins-operator'
@@ -16,44 +33,44 @@ def call(String repoUrl, String branchName, String directoryName, String project
 			}
 		}
 	   }
-// 	    stage("Build") {
-// 		  agent {
-// 		    docker {
-// 		       image 'maven:3-alpine'
-// 		       args '-v $HOME/.m2:/root/.m2'
-// 		       reuseNode true
-// 		    }
-// 		  }
-// 		  steps {
-// 		     script{STAGE_NAME="Build Stage"}
-// 		     dir ("${directoryName}") {
-// 			sh 'mvn clean install -DskipTests '
-// 		     }
-// 		  }
-// 	   }
+	    stage("Build") {
+		  agent {
+		    docker {
+		       image 'maven:3-alpine'
+		       args '-v $HOME/.m2:/root/.m2'
+		       reuseNode true
+		    }
+		  }
+		  steps {
+		     script{STAGE_NAME="Build Stage"}
+		     dir ("${directoryName}") {
+			sh 'mvn clean install -DskipTests '
+		     }
+		  }
+	   }
            
-//            stage("Unit Test") {
-// 		   agent {
-// 		      docker {
-// 			image 'maven:3-alpine'
-// 			args '-v $HOME/.m2:/root/.m2'
-// 			reuseNode true
-// 		      }
-// 		  }
-// 		  steps {
-// 			script{STAGE_NAME="Unit Test"}
-// 			dir ("${directoryName}") {
-// 				sh 'mvn test'
-// 				sh 'mvn speedy-spotless:install-hooks'
-// 				sh 'mvn speedy-spotless:check'
-// 			}
-// 		  }
-// 		  post {
-// 		        always {
-// 			        junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
-// 			}
-// 		  }
-// 	   }
+           stage("Unit Test") {
+		   agent {
+		      docker {
+			image 'maven:3-alpine'
+			args '-v $HOME/.m2:/root/.m2'
+			reuseNode true
+		      }
+		  }
+		  steps {
+			script{STAGE_NAME="Unit Test"}
+			dir ("${directoryName}") {
+				sh 'mvn test'
+				sh 'mvn speedy-spotless:install-hooks'
+				sh 'mvn speedy-spotless:check'
+			}
+		  }
+		  post {
+		        always {
+			        junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+			}
+		  }
+	   }
 	   stage("Sonar Report") {
 		   agent {
 			docker {
